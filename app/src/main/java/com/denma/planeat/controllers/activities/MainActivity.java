ac4a6deb4dc90024dbb8b426060e8764cc2c413c
Denmaaa <anthony.mineo.pro@Gmail.com>
2019-02-01 15:26:28 +0100

    Add navigation view + toolbar

--- app/src/main/java/com/denma/planeat/controllers/activities/MainActivity.java
+++ app/src/main/java/com/denma/planeat/controllers/activities/MainActivity.java
@@ -2,16 +2,36 @@ package com.denma.planeat.controllers.activities;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.denma.planeat.R;
 import com.denma.planeat.controllers.BaseActivity;
 
-public class MainActivity extends BaseActivity {
+import butterknife.BindView;
+
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    // FOR DESIGN
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.activity_main_drawer_layout)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.activity_main_nav_view)
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
+        this.configureToolBar();
+        this.configureDrawerLayout();
+        this.configureNavigationView();
     }
 
     // --------------------
@@ -21,5 +41,49 @@ public class MainActivity extends BaseActivity {
     @Override
     protected int getActivityLayout() {
         return R.layout.activity_main;
+    }
+
+    // --------------------
+    // CONFIGURATIONS
+    // --------------------
+
+    // - Configure Toolbar
+    private void configureToolBar() {
+        setSupportActionBar(toolbar);
+    }
+
+    // - Configure Drawer Layout
+    private void configureDrawerLayout() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    // - Configure NavigationView
+    private void configureNavigationView() {
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    // --------------------
+    // ACTIONS
+    // --------------------
+
+    // --------------------
+    // MENUS
+    // --------------------
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    // - Handle back click to close menu
+    @Override
+    public void onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
